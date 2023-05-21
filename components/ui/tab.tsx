70a8070b64ed73f64ba3c2f72c295f4d714a6459
Wang Yi <1721436982@qq.com>
2023-05-21 15:19:39 +0800

    feat: add daisyui

--- components/ui/tab.tsx
+++ components/ui/tab.tsx
@@ -1,19 +1,62 @@
-import type {FC} from 'react';
-import React, {memo } from 'react';
+import type { FC } from 'react';
+import React, { memo } from 'react';
+import { BaseFormProps } from '@/utils/baseProps'
+import usePropsValue from '@/hooks/usePropsValue'
+import { mergeProps } from '@/utils/mergeProps'
+
+interface TabItemProps {
+  label: React.ReactNode
+  selected: boolean
+  onClick: () => void
+}
+
+const TabItem: FC<TabItemProps> = memo((props) => {
+  const { selected , label } = props
+  return (
+    <a className={`tab ${selected && 'tab-active'}`}>{label}</a>
+  );
+})
+
+TabItem.displayName = 'TabItem'
 
 export interface Item {
-  key: React.Key;
+  value: React.Key;
   label: React.ReactNode;
 }
 
-export interface TabProps {
-
+export interface TabProps extends BaseFormProps<React.Key> {
+  items: Item[]
 }
 
-const Tab: FC<TabProps> = memo(() => {
+const defaultProps = {
+  defaultValue: []
+}
+
+const Tab: FC<TabProps> = memo((p) => {
+  const props = mergeProps(p, defaultProps)
+  const { items } = props
+  const [ value, onChange ] = usePropsValue(props)
   return (
-    <div>Tab</div>
+    <div className="tabs tabs-boxed">
+      {
+        items.map((item, index) => {
+          return (
+            <TabItem
+              label={item.label}
+              selected={value === item.value}
+              key={value}
+              onClick={() => {
+                if (value === item.value) return
+                onChange(item.value)
+              }}
+            />
+          )
+        })
+      }
+    </div>
   );
 })
+
+Tab.displayName = 'Tab'
 
 export default Tab;
--- components/ui/tab/index.ts
+++
@@ -1,6 +0,0 @@
-import { default as Tab } from './tab'
-import { default as Item } from './item'
-
-export default Object.assign(Tab, {
-  Item,
-})
