31b819a933895c8170d8396999239b1730406e71
Wang Yi <1721436982@qq.com>
2023-05-23 23:19:12 +0800

    feat: add project about page

---
+++ pages/about/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+type Props = {};
+
+/**
+ * @Author: wangyi
+ * @Date: 2023-05-23
+ */
+const About: React.FC<Props> = () => {
+  return (
+    <div>About</div>
+  );
+}
+
+export default About;
---
+++ pages/project/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+type Props = {};
+
+/**
+ * @Author: wangyi
+ * @Date: 2023-05-23
+ */
+const Project: React.FC<Props> = () => {
+  return (
+    <div>Project</div>
+  );
+}
+
+export default Project;
