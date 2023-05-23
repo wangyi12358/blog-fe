2e3ed4451be74133502d3487e96a1bdbc06703aa
Wang Yi <1721436982@qq.com>
2023-05-23 20:59:41 +0800

    feat: personal info

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import Avatar from "@/components/ui/avatar";
 
 type Props = {};
 
 const PersonalInfo = () => {
   return (
-    <div className="flex items-center justify-center mt-6 relative layout-container rounded-sm p-6">
+    <div className="flex items-center justify-center mt-6 relative layout-container rounded-sm p-8">
       <div className="layout-background absolute" />
-      <div>
-        123
+      <div className="flex flex-col items-center">
+        <Avatar src="https://mika-resource.oss-cn-hangzhou.aliyuncs.com/blog/avatar.jpg"/>
+        <div className="text-center mt-2">@Mika_wang</div>
+        <h2 className="text-center text-white font-bold">Mika wang</h2>
       </div>
     </div>
   )
