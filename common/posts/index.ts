6f174728a134c75b31c2df62362a0eb45591f496
Wang Yi <1721436982@qq.com>
2023-05-27 23:25:33 +0800

    feat: add go escape

--- common/posts/index.ts
+++ common/posts/index.ts
@@ -9,12 +9,19 @@ export interface Post {
 // all post
 export const ALL_POSTS: Post[] = [
   {
-    title: 'Web3',
-    desc: '什么是web3？什么是区块链？什么是DApp？什么是智能合约？web3需要了解什么知识？接下来我会带大家一起探索web3。',
-    filename: 'web3',
+    title: 'Go 逃逸分析（还在编写中）',
+    desc: '什么是逃逸分析？什么是指针逃逸？堆栈是什么？，接下来带大家一起探索。',
+    filename: 'go-escape',
     date: '2023-5-27',
-    readTime: 3
-  }
+    readTime: 5
+  },
+  // {
+  //   title: 'Web3',
+  //   desc: '什么是web3？什么是区块链？什么是DApp？什么是智能合约？web3需要了解什么知识？接下来我会带大家一起探索web3。',
+  //   filename: 'web3',
+  //   date: '2023-5-27',
+  //   readTime: 3
+  // }
 ]
 
 // featured posts
